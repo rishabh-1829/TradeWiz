f0c814c6fb8987c3390a805361f75b46c21d036b
rishabh <rishabh4538@gmail.com>
2025-08-17 16:03:27 +0530

    Update Brokage.js
    spelling correction

--- frontend/src/landing_page/pricing/Brokage.js
+++ frontend/src/landing_page/pricing/Brokage.js
@@ -8,7 +8,7 @@ function Brokage() {
             <a href='#' style={{textDecoration:"none"}}><h3>Brokerage calculator</h3></a>
             
                 <ul style={{textAlign:"left"}}>
-                    <li>₹15.34 per scrip (₹3.5 CDSL fee + ₹9.5 Zerodha fee + ₹2.34 GST) is charged on the trading account ledger when stocks are sold, irrespective of quantity.</li>
+                    <li>₹15.34 per scrip (₹3.5 CDSL fee + ₹9.5 TradeWiz fee + ₹2.34 GST) is charged on the trading account ledger when stocks are sold, irrespective of quantity.</li>
                     <li>MTF Interest: 0.04% per day (₹40 per lakh) on the funded amount. The interest is applied from T+1 day until the day MTF stocks are sold.</li>
                     <li>MTF Brokerage: 0.03% or Rs. 20/executed order, whichever is lower.</li>
                     <li>Female demat account holders (as first holder) will enjoy a discount of ₹0.25 per transaction on the CDSL fee</li>
